9583e2dad88d58124751d2b9f0167b0c534c5bad
YuverlyHidokun <yuverlyverdezoto@gmail.com>
2025-06-14 21:51:08 -0500

    el segundo

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// Carga variables del .env
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: parseInt(process.env.VITE_PORT) || 5173,
-    host: '0.0.0.0' // importante si estás desplegando en Render
+    host: '0.0.0.0'
+  },
+  base: '/', // Asegura rutas correctas
+  build: {
+    rollupOptions: {
+      input: './index.html'
+    }
   }
 });
